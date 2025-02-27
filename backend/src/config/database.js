01a26a08eb62398f9c8f0adb58c754da485e85d6
Ryan MacMurray <macmurray_ryan@bah.com>
2025-02-27 12:52:17 -0500

    Still trying to get proxy working

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,7 +2,7 @@ const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 
 const db = new sqlite3.Database(
-  path.resolve(__dirname, "../data/CovidData.db"),
+  path.resolve(__dirname, "../../data/CovidData.db"),
   (err) => {
     if (err) {
       console.error("Database connection error:", err.message);

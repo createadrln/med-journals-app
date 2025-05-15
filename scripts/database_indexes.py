6fd2a7618c73054d0fb70832c9414b90a7a35682
Ryan MacMurray <ryan@createadrenaline.com>
2025-05-15 15:37:44 -0400

    Separate

--- scripts/database_indexes.py
+++ scripts/database_indexes.py
@@ -3,13 +3,13 @@ import sqlite3
 conn = sqlite3.connect('/app/databases/CovidData.db')
 cursor = conn.cursor()
 
-add_research_table_indexes = """
-    CREATE INDEX idx_id ON covid_research (id);
-    CREATE INDEX idx_source ON covid_research (source);
-    CREATE INDEX idx_date ON covid_research (pub_date);
-"""
+add_research_table_id_index = "CREATE INDEX idx_id ON covid_research (id);"
+add_research_table_source_index = "CREATE INDEX idx_source ON covid_research (source);"
+add_research_table_date_index = "CREATE INDEX idx_date ON covid_research (pub_date);"
 
-cursor.execute(add_research_table_indexes)
+cursor.execute(add_research_table_id_index)
+cursor.execute(add_research_table_source_index)
+cursor.execute(add_research_table_date_index)
 
 conn.commit()
 conn.close()
